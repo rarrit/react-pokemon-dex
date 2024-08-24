8a512eefcd1ebc93019d966a887b6aa58ed3877e
김민규 <tjfvndfud@gmail.com>
2024-08-25 04:33:05 +0900

    배포용 코드 적용

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/react-pokemon-dex",
+  base: "/react-pokemon-dex/",
   resolve: {
     alias: [
       {
