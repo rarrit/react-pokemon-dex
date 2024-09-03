9067fa2576571b0f1dbaa2ec0c4a2957bd41c065
김민규 <tjfvndfud@gmail.com>
2024-09-04 05:46:06 +0900

    fix: 수정

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,57 +1,41 @@
 import { useNavigate } from "react-router-dom";
 import { TypeAttr, TypeBg } from "../assets/js/types";
-import styled from "styled-components";
 import { useDispatch, useSelector } from "react-redux";
 import { addPokemon, removePokemon } from "@/feature/pokemons/pokemonsSlice";
+import styled from "styled-components";
+import imgPokeball from "../assets/img/img-pokemonball.png";
 
 
-const PokemonCard = ({ 
-  pokemon, // 포켓몬 객체
-  isSelected // 추가 여부 체크
-}) => {
-
+const PokemonCard = ({ pokemon  }) => {
   const dispatch = useDispatch();
-  const selectPokemonList = useSelector(state => state.pokemons.selectedPokemonList);
+  const navigate = useNavigate();  
+  const selectPokemonList = useSelector(state => state.pokemons.selectPokemonList);
+  const isSelected = selectPokemonList.find(list => list.id === parseInt(pokemon.id));
   const filterType = TypeAttr(pokemon.types[0]);
   const pokemonBg = TypeBg(filterType);    
-  const navigate = useNavigate();
-  
-  const handleDetailClick = () => {    
-    navigate(`/detail/${pokemon.id}`, {
-      state: {
-        selectedPokemon: pokemon,
-        selectedPokemonList: selectPokemonList, // 선택된 포켓몬 리스트를 함께 전달
-      },
-    });
-  };
-
-  const handleAddPokemon = (pokemon) => {
-    dispatch(addPokemon(pokemon));
-    // console.log("추가", pokemon);
-  }
-  const handleRemovePokemon = (pokemon) => {
-    dispatch(removePokemon(pokemon));
-  }
-
+    
+  const handleDetailClick = () => navigate(`/detail/${pokemon.id}`);
+  const handleAddPokemon = (pokemon) => dispatch(addPokemon(pokemon));    
+  const handleRemovePokemon = (pokemon) => dispatch(removePokemon(pokemon));
 
   return (
     <>
-      <CardItem $filterBg={pokemonBg}>
-        <ImgBox $imgUrl={pokemon.img_url}>
-          <button onClick={handleDetailClick}></button>
-        </ImgBox>
+      <CardItem 
+        $filterBg={pokemonBg} 
+        className={!isSelected ? "" : "selected"}
+        onClick={() => isSelected ? handleRemovePokemon(pokemon) : handleAddPokemon(pokemon)}        
+      >
+        <ImgBox $imgUrl={pokemon.img_url} />
         <TextBox>
           <p className="pokemonName">{pokemon.korean_name}</p>
           <p className="pokemonDesc">{pokemon.description}</p>
         </TextBox>        
         <ButtonArea>
           <div className="inner">
-            <button onClick={handleDetailClick}>DETAIL</button>
-            {isSelected ? (
-              <Button onClick={() => handleRemovePokemon(pokemon)}>REMOVE</Button>
-            ) : (
-              <Button onClick={() => handleAddPokemon(pokemon)}>ADD</Button>
-            )}
+            <Button onClick={(e) => {              
+              e.stopPropagation();
+              handleDetailClick();
+            }}>자세히 보기</Button>
           </div>          
         </ButtonArea>
         
@@ -65,18 +49,48 @@ export default PokemonCard
 const CardItem = styled.div`  
   position:relative;
   display: flex;
-  flex-direction: column;  
-  width:calc(20% - 20px);
-  height:420px;
-  background: url(${props => props.$filterBg}) no-repeat;
-  background-size:100% 100%;
+  flex-direction: column;
+  justify-content: space-between;
+  width:180px;
+  height:253px;  
+  background:#fff;
+  cursor:pointer;
+  &:before {
+    content:'';
+    position:absolute;
+    top:0;
+    left:0;
+    width:100%;
+    height:100%;
+    background: url(${props => props.$filterBg}) no-repeat;
+    background-size:100% 100%;
+    z-index:1;
+  }
+  &.selected {
+    &:after {
+      content: '';
+      position: absolute;
+      z-index: 2;
+      top: 16px;
+      left: 4px;
+      width: 29px;
+      height: 29px;
+      background: url(${imgPokeball}) no-repeat center center;
+      background-size:cover;
+      border-radius: 100%;
+    }
+  }  
+  > div {
+    position:relative;
+    z-index:2;
+  }
 `;
 
 const ImgBox = styled.div`
-  width: 100%; /* 원하는 너비 */
-  height: 230px; /* 원하는 높이 */
-  background: url(${props => props.$imgUrl}) no-repeat center center;
-  background-size: 140px;
+  width: 100%; 
+  height: 120px;
+  background: url(${props => props.$imgUrl}) no-repeat bottom center;
+  background-size: 96px;
   a,button {
     display: block;
     width: 100%;
@@ -87,33 +101,25 @@ const ImgBox = styled.div`
   }
 `;
 
-const ButtonArea = styled.div`  
-  position: absolute;
-  top: 165px;
-  left: 0;
-  z-index: 1;
-  padding: 0 30px;
+const ButtonArea = styled.div` 
+  padding: 0 30px 20px;
   width: 100%;
   box-sizing: border-box;
   .inner {
     display:flex;  
-    background:#f5483d;    
-    border:1px solid #000;
+    background:rgba(255,255,255,.5);    
+    box-shadow: .5px .5px 5px rgba(255,255,255,1);    
+    overflow: hidden;
     > * {
       flex: 1;
       display:flex;
       align-items: center;
       justify-content: center;
-      height:25px;
+      height:20px;
       text-decoration: none;  
       background:transparent; 
       border:none; 
       cursor: pointer;  
-      color:#ffcc1c;  
-      &:first-child {
-        color:#f5483d;
-        background:#d0d9de;
-      }
     }
   }  
 `
@@ -122,15 +128,15 @@ const Button = styled.button`
 `;
 
 const TextBox = styled.div`
-  padding:0 40px;
+  padding:15px 30px;
   .pokemonName {
-    font-size:24px;
+    font-size:16px;
     font-weight:bold;
     color:#000;
   }
   .pokemonDesc {
     margin:10px 0 0 0;
-    font-size:16px;
+    font-size:14px;
     color:#212121;
   }
 `
